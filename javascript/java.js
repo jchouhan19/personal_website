30a5a7ff4f97ace3335f75fab91e226f19831d25
Jugraj Chouhan <jugraj_chouhan@hotmail.com>
2020-11-24 19:28:18 -0800

    Lab 7 with Javascript functions

--- javascript/java.js
+++ javascript/java.js
@@ -1,21 +1,63 @@
-const courselist = [course_A = { code: 1620, name: "Web Fundamental Technologies" },
-                    course_B = {code: 1630, name: "Database Systems" },
-                    course_C ={code: 1420,  name: "Intro to Sys Admin"}];
 
-let code;
-let course_confirm = false;
-do { 
-    code = window.prompt("Enter your 4 digit course code: ");
-  } while (isNaN(Number(code))||code.length !== 4 )
-
-for(let x of courselist){
-    if (x.code===Number(code)){
-        alert(`Yes I am taking the course: ACIT${x.code}-${x.name}`)
-        course_confirm = true;
+function create_course_array() {
+    let courselist = []
+    let course = {}
+    let search_a = document.querySelectorAll("section div a")
+    let search_d = document.querySelector(".web_date")
+    for (object of search_a) {
+        course = { code: object.innerHTML, date: search_d.innerHTML }
+        courselist.push(course)
     }
 
+    return courselist
 }
-if (! course_confirm) {
-    courselist.push(course_D ={code: Number(code), name: null})
-    alert(`Course ACIT${Number(code)} entered successfully`)
+
+function findCourse(courselist) {
+    console.log(courselist)
+    let code_1;
+    let course_confirm = false;
+    do {
+        code_1 = window.prompt("Enter your 4 digit course code: ");
+    } while (isNaN(Number(code_1)) || code_1.length !== 4) 
+    for (course in courselist) {
+        for (co in courselist[course])
+       { 
+        console.log(courselist[course][co])
+        if (courselist[course][co].includes(String(code_1))) {
+            let c = document.getElementsByClassName('c'+ String(code_1))
+            c[0].style.backgroundColor = 'green'
+            c[1].style.backgroundColor = 'green'
+            c[2].style.backgroundColor = 'green'
+            course_confirm = true;
+        }}
+    }
+    if ((course_confirm === false)) {
+        let course_new = document.createElement('div')
+        course_new.innerHTML = code_1 + '<br> Fall 2020'
+        let section = document.getElementById('course')
+        section.appendChild(course_new)
+
+        let course_desc = document.createElement('div')
+        course_desc.innerHTML = 'N/A'
+        let section_desc = document.getElementById('course')
+        section_desc.appendChild(course_desc)
+
+
+    }
 }
+
+
+let courselist = create_course_array()
+findCourse(courselist)
+
+// // for (let x of courselist) {
+//     // if (x.code === Number(code)) {
+//         alert(`Yes I am taking the course: ACIT${x.code}-${x.name}`)
+//         course_confirm = true;
+//     // }
+
+// // }
+// // if (!course_confirm) {
+//     // courselist.push(course_D = { code: Number(code), name: null })
+//     // alert(`Course ACIT${Number(code)} entered successfully`)
+// // }
